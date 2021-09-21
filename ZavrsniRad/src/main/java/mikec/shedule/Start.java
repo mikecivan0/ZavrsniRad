6f0fd985f5533b89c3f1d8626707dd3ff12cb790
Ivan Mikec <mikecivan0@gmail.com>
2021-09-22 00:31:34 +0200

    removed MainScreen form, removed Test data

--- ZavrsniRad/src/main/java/mikec/shedule/Start.java
+++ ZavrsniRad/src/main/java/mikec/shedule/Start.java
@@ -2,15 +2,13 @@ package mikec.shedule;
 
 //import mikec.shedule.util.BaseValues;
 //import mikec.shedule.util.HibernateUtil;
-//import mikec.shedule.util.TestData;
+
 import mikec.shedule.view.SplashScreen;
 public class Start {
     
     public static void main(String[] args) {
 //        BaseValues baseValues = new BaseValues(HibernateUtil.getSession());
-//        baseValues.load();
-//        TestData.load();  
+//        baseValues.load(); 
         new SplashScreen().setVisible(true);
-    }
-    
+    }    
 }
