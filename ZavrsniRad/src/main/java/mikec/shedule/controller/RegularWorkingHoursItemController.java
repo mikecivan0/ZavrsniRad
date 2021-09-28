37a9dac40ef2a3441edb6104530fc59514c0d0a4
Ivan Mikec <mikecivan0@gmail.com>
2021-09-28 13:04:57 +0200

    created RegularWorkingHoursItemController

---
+++ ZavrsniRad/src/main/java/mikec/shedule/controller/RegularWorkingHoursItemController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mikec.shedule.controller;
+
+
+import java.util.List;
+import mikec.shedule.model.RegularWorkingHoursItem;
+import mikec.shedule.util.BaseException;
+
+/**
+ *
+ * @author Ivan
+ */
+public class RegularWorkingHoursItemController extends BaseController<RegularWorkingHoursItem>{
+
+    public RegularWorkingHoursItemController() throws BaseException {
+        super();
+    }  
+
+    @Override
+    public List<RegularWorkingHoursItem> read() {
+        return session.createQuery("from regularWorkingHoursItems").list();
+    }
+
+    @Override
+    protected void createControll() throws BaseException {
+           
+    }
+
+    @Override
+    protected void updateControll() throws BaseException {
+      
+    }
+
+    @Override
+    protected void deleteControll() throws BaseException {
+       
+    }
+    
+    public RegularWorkingHoursItem find(int i){
+        return (RegularWorkingHoursItem) session.createQuery(
+                "from regularWorkingHoursItems where id=:id")
+                .setParameter("id", i)
+                .uniqueResult();
+    }
+    
+   
+ 
+}
