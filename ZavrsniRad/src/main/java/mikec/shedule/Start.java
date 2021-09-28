fdd789b43c04b2968bf0d0fe9080be555f200196
Ivan Mikec <mikecivan0@gmail.com>
2021-09-29 01:33:28 +0200

    added throw exception

--- ZavrsniRad/src/main/java/mikec/shedule/Start.java
+++ ZavrsniRad/src/main/java/mikec/shedule/Start.java
@@ -1,5 +1,7 @@
 package mikec.shedule;
 
+//import javax.swing.JOptionPane;
+//import mikec.shedule.util.BaseException;
 //import mikec.shedule.util.BaseValues;
 //import mikec.shedule.util.HibernateUtil;
 
@@ -7,8 +9,13 @@ import mikec.shedule.view.SplashScreen;
 public class Start {
     
     public static void main(String[] args) {
-//        BaseValues baseValues = new BaseValues(HibernateUtil.getSession());
-//        baseValues.load(); 
-        new SplashScreen().setVisible(true);
+//        try {
+//            BaseValues baseValues = new BaseValues(HibernateUtil.getSession()); 
+//            baseValues.load();
+//
+//        } catch (BaseException ex) {
+//           JOptionPane.showMessageDialog(null, ex.getMessage());
+//        }
+                new SplashScreen().setVisible(true);
     }    
 }
